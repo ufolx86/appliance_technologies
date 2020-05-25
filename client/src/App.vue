433784b811b1989f27dc343d686e10441dfa8d21
biktokai86 <victor.r.urueta@gmail.com>
2020-05-25 13:20:55 -0500

    Configure Router

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/test">Test</router-link>
+      <router-link to="/test">Charts</router-link>
     </div>
     <router-view/>
   </div>
---
+++ client/src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>{{ msg }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Home',
+        data() {
+            return {
+                msg: "WELCOME TO THIS DEMO PLEASE CLICK ON THE NAV BAR AND GO SEE A NICE CHART"
+            };
+        },
+        methods: {
+            
+        },
+    };
+</script>
