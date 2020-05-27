97f593a147ba0d4b742b0b95a29e6539e66a0a3a
biktokai86 <victor.r.urueta@gmail.com>
2020-05-27 17:07:30 -0500

    Included second function to return a numpy array

--- server/app.py
+++ server/app.py
@@ -17,16 +17,22 @@ app.config.from_object(__name__)
 CORS(app, resources={r'/*': {'origins': '*'}})
 
 # sanity check route
-@app.route('/test', methods=['GET'])
+@app.route('/bitcoinPrices', methods=['GET'])
 def returnDataSeries():
     #bitcoinPrices = np.zeros(4000,2)
     range_begin = datetime.datetime(2020, 1, 1, 19, 39, 36, 815417)
-    range_end = datetime.datetime(2020, 5, 25, 19, 39, 36, 815417)
+    range_end = datetime.datetime(2020, 5, 26, 19, 39, 36, 815417)
     b = BtcConverter()
     #get_previous_price(currency,date begin, date end)
     bitcoinPrices = b.get_previous_price_list('EUR', range_begin, range_end)
     return jsonify(bitcoinPrices)
     #return jsonify('pong!')
+@app.route('/numpyArray', methods=['GET'])
+def returnNumpyArray():
+    npSeries = np.random.random((2,100))
+    listSeries = npSeries.tolist()
+    return jsonify(listSeries)
+    # return "TEST_STRING"
 
 if __name__ == '__main__':
     app.run(debug=True)
